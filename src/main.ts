05ea5da91127fa18cbf98af0054de6bfcd8b96bb
Giullia <giulliachiarotti@gmail.com>
2025-07-17 10:25:12 -0400

    atualizações

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 import "./style.css";
 
 console.log("Portal IFRO carregado.");
+
+const menuBtn = document.getElementById('menu-btn') as HTMLButtonElement;
+const nav = document.getElementById('nav-links') as HTMLElement;
+
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+
